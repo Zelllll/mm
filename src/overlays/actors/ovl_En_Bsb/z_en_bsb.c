47e2b0e28d82e6b6b479f28b43b35869b8a9444e
Zelllll <56516451+Zelllll@users.noreply.github.com>
2023-11-02 21:22:00 -0500

    data migrated

--- src/overlays/actors/ovl_En_Bsb/z_en_bsb.c
+++ src/overlays/actors/ovl_En_Bsb/z_en_bsb.c
@@ -17,27 +17,8 @@ void EnBsb_Destroy(Actor* thisx, PlayState* play);
 void EnBsb_Update(Actor* thisx, PlayState* play);
 void EnBsb_Draw(Actor* thisx, PlayState* play);
 
-void func_80C0BFE8(EnBsb* this, PlayState* play);
-void func_80C0C238(EnBsb* this, PlayState* play);
-void func_80C0C364(EnBsb* this, PlayState* play);
-void func_80C0C484(EnBsb* this, PlayState* play);
-void func_80C0C6A8(EnBsb* this, PlayState* play);
-void func_80C0C8EC(EnBsb* this, PlayState* play);
-void func_80C0CB3C(EnBsb* this, PlayState* play);
-void func_80C0CD04(EnBsb* this, PlayState* play);
-void func_80C0CDE4(EnBsb* this, PlayState* play);
-void func_80C0CFDC(EnBsb* this, PlayState* play);
-void func_80C0D10C(EnBsb* this, PlayState* play);
-void func_80C0D27C(EnBsb* this, PlayState* play);
-void func_80C0D384(EnBsb* this, PlayState* play);
-void func_80C0D51C(EnBsb* this, PlayState* play);
-void func_80C0D9B4(EnBsb* this, PlayState* play);
-void func_80C0DB18(EnBsb* this, PlayState* play);
-void func_80C0E1C0(EnBsb* this, PlayState* play);
-void func_80C0E480(EnBsb* this, PlayState* play);
-void func_80C0E4FC(EnBsb* this, PlayState* play);
+static f32 D_80C0F8D0 = 0.0f;
 
-#if 0
 // static ColliderJntSphElementInit sJntSphElementsInit[7] = {
 static ColliderJntSphElementInit D_80C0F8D4[7] = {
     {
@@ -73,7 +54,7 @@ static ColliderJntSphElementInit D_80C0F8D4[7] = {
 // static ColliderJntSphInit sJntSphInit = {
 static ColliderJntSphInit D_80C0F9D0 = {
     { COLTYPE_HIT6, AT_ON | AT_TYPE_ENEMY, AC_ON | AC_TYPE_PLAYER, OC1_ON | OC1_TYPE_PLAYER, OC2_TYPE_1, COLSHAPE_JNTSPH, },
-    ARRAY_COUNT(sJntSphElementsInit), D_80C0F8D4, // sJntSphElementsInit,
+    ARRAY_COUNT(D_80C0F8D4), D_80C0F8D4, // sJntSphElementsInit,
 };
 
 // static DamageTable sDamageTable = {
@@ -124,123 +105,296 @@ ActorInit En_Bsb_InitVars = {
     /**/ EnBsb_Draw,
 };
 
-#endif
-
-extern ColliderJntSphElementInit D_80C0F8D4[7];
-extern ColliderJntSphInit D_80C0F9D0;
-extern DamageTable D_80C0F9E0;
-
 extern UNK_TYPE D_06000C50;
 extern UNK_TYPE D_06004894;
 
+
+u32 D_80C0FA20[] = {
+0x060086BC,
+0x0600CD88,
+0x06000400,
+0x060065D8,
+0x06000FF0,
+0x06000C50,
+0x06006C48,
+0x06001390,
+0x06002AF4,
+0x06002590,
+0x06007120,
+0x060043A4,
+0x06007B18,
+0x06001CD8,
+0x06003E1C,
+0x06003238,
+0x0600606C,
+0x06005440,
+0x06004E2C,
+0x06004894,
+0x06004208,
+0x0600D3CC,
+0x06004510,
+0x06001F90,
+0x0600C790,
+};
+
+
+u32 D_80C0FA84[] = {
+0x02000200,
+0x00020000,
+0x02020002,
+0x00000200,
+0x02000200,
+0x00000202,
+0x02000000,
+};
+
+u32 D_80C0FAA0[] = {
+0x00000000,
+0x41000000,
+0x00000000,
+};
+
+u32 D_80C0FAAC[] = {
+0x00000000,
+0xBFC00000,
+0x00000000,
+};
+
+u32 D_80C0FAB8[] = {
+0x2EE0D8F0,
+0xB5C80000,
+};
+
+u32 D_80C0FAC0[] = {
+0xD8F02710,
+0xD8F00000,
+};
+
+u32 D_80C0FAC8[] = {
+0xC2A00000,
+0x43ED8000,
+0xC511A000,
+0x00000000,
+0x00000000,
+0x00000000,
+};
+
+// void mode_anime_set(EnBsb* this, int i);
+void func_80C0B290(EnBsb* this, s32 arg1);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0B290.s")
 
+// void dust_ground_setx(PlayState* play, EnBsb* this, xyz_t* pos);
+void func_80C0B31C(PlayState* play, EnBsb* this, Vec3f* pos);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0B31C.s")
 
+// void En_Bsb_actor_ct(ACTOR* thisx, game_t* game);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/EnBsb_Init.s")
 
+// void En_Bsb_actor_dt(ACTOR* thisx, game_t* game);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/EnBsb_Destroy.s")
 
+// int mode_attack_check(EnBsb* this, PlayState* play);
+s32 func_80C0B888(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0B888.s")
 
+// void mode_doshin_check(EnBsb* this, PlayState* play);
+void func_80C0B970(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0B970.s")
 
+// void mode_attack_doshin_check(EnBsb* this, PlayState* play);
+void func_80C0BA58(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0BA58.s")
 
+// int mode_hand_check(EnBsb* this);
+s32 func_80C0BC30(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0BC30.s")
 
+// void camera_move(EnBsb* this, PlayState* play);
+void func_80C0BE1C(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0BE1C.s")
 
+// void mode_ocarina_check_wait_init(EnBsb* this);
+void func_80C0BF2C(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0BF2C.s")
 
+// void mode_ocarina_check_wait(EnBsb* this, PlayState* play);
+void func_80C0BFE8(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0BFE8.s")
 
+// void mode_event_start_init(EnBsb* this, PlayState* play);
+void func_80C0C0F4(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C0F4.s")
 
+// void mode_event_start(EnBsb* this, PlayState* play);
+void func_80C0C238(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C238.s")
 
+// void mode_demo_stand_up_init(EnBsb* this);
+void func_80C0C32C(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C32C.s")
 
+// void mode_demo_stand_up(EnBsb* this, PlayState* play);
+void func_80C0C364(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C364.s")
 
+// void mode_rail_walk_init(EnBsb* this);
+void func_80C0C430(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C430.s")
 
+// void mode_rail_walk(EnBsb* this, PlayState* play);
+void func_80C0C484(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C484.s")
 
+// void mode_ouch_init(EnBsb* this);
+void func_80C0C610(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C610.s")
 
+// void mode_ouch(EnBsb* this, PlayState* play);
+void func_80C0C6A8(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C6A8.s")
 
+// void mode_attack_move_init(EnBsb* this);
+void func_80C0C86C(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C86C.s")
 
+// void mode_attack_move(EnBsb* this, PlayState* play);
+void func_80C0C8EC(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0C8EC.s")
 
+// void mode_attack_jump_init(EnBsb* this, PlayState* play);
+void func_80C0CA28(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CA28.s")
 
+// void mode_attack_jump(EnBsb* this, PlayState* play);
+void func_80C0CB3C(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CB3C.s")
 
+// void mode_attack_jump_wait_init(EnBsb* this);
+void func_80C0CCCC(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CCCC.s")
 
+// void mode_attack_jump_wait(EnBsb* this, PlayState* play);
+void func_80C0CD04(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CD04.s")
 
+// void mode_attack_init(EnBsb* this);
+void func_80C0CD90(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CD90.s")
 
+// void mode_attack(EnBsb* this, PlayState* play);
+void func_80C0CDE4(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CDE4.s")
 
+// void mode_hirumu_init(EnBsb* this);
+void func_80C0CF4C(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CF4C.s")
 
+// void mode_hirumu(EnBsb* this, PlayState* play);
+void func_80C0CFDC(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0CFDC.s")
 
+// void mode_damage_init(EnBsb* this);
+void func_80C0D00C(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D00C.s")
 
+// void mode_damage(EnBsb* this, PlayState* play);
+void func_80C0D10C(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D10C.s")
 
+// void mode_paralyze_init(EnBsb* this);
+void func_80C0D214(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D214.s")
 
+// void mode_paralyze(EnBsb* this, PlayState* play);
+void func_80C0D27C(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D27C.s")
 
+// void mode_laugh_init(EnBsb* this);
+void func_80C0D334(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D334.s")
 
+// void mode_laugh(EnBsb* this, PlayState* play);
+void func_80C0D384(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D384.s")
 
+// void mode_dead_init(EnBsb* this, PlayState* play);
+void func_80C0D3C0(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D3C0.s")
 
+// void mode_dead(EnBsb* this, PlayState* play);
+void func_80C0D51C(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D51C.s")
 
+// void mode_dosun_init(EnBsb* this, PlayState* play);
+void func_80C0D964(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D964.s")
 
+// void mode_dosun(EnBsb* this, PlayState* play);
+void func_80C0D9B4(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0D9B4.s")
 
+// void mode_goal_down_init(EnBsb* this);
+void func_80C0DA58(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0DA58.s")
 
+// void mode_goal_down(EnBsb* this, PlayState* play);
+void func_80C0DB18(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0DB18.s")
 
+// void mode_demo01_init(EnBsb* this);
+void func_80C0E178(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E178.s")
 
+// void mode_demo01(EnBsb* this, PlayState* play);
+void func_80C0E1C0(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E1C0.s")
 
+// void mode_demo02_init(EnBsb* this);
+void func_80C0E3B8(EnBsb* this);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E3B8.s")
 
+// void mode_demo02(EnBsb* this, PlayState* play);
+void func_80C0E480(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E480.s")
 
+// void mode_demo03(EnBsb* this, PlayState* play);
+void func_80C0E4FC(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E4FC.s")
 
+// void damage_proc(EnBsb* this, PlayState* play);
+void func_80C0E618(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E618.s")
 
+// int move_demoproc(EnBsb* this, PlayState* play);
+s32 func_80C0E9CC(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0E9CC.s")
 
+// void En_Bsb_actor_move(ACTOR* thisx, game_t* game);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/EnBsb_Update.s")
 
+// int bsb_display1(game_t* game, int jyoint_no, Gfx** shape, xyz_t* t, s_xyz* r, void* p);
+s32 func_80C0EEA0(GameState* gameState, s32 arg1, Gfx** arg2, Vec3f* arg3, Vec3s* arg4, void* arg5);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0EEA0.s")
 
+// int bsb_display3(game_t* game, int jyoint_no, Gfx** shape, xyz_t* t, s_xyz* r, void* p, Gfx** next_disp);
+s32 func_80C0F078(GameState* gameState, s32 arg1, Gfx** arg2, Vec3f* arg3, Vec3s* arg4, void* arg5, Gfx** arg6);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0F078.s")
 
+// void bsb_display2(game_t* game, int jyoint_no, Gfx** shape, s_xyz* r, void* p);
+void func_80C0F170(GameState* gameState, s32 arg1, Gfx** arg2, Vec3s* arg3, void* arg4);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0F170.s")
 
+// void En_Bsb_actor_draw(ACTOR* thisx, game_t* game);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/EnBsb_Draw.s")
 
+// void en_bsb_eff_set(EnBsb* this, xyz_t* pos, xyz_t* vec, xyz_t* acc, float scale, short timer);
+void func_80C0F544(EnBsb* this, Vec3f* pos, Vec3f* velocity, Vec3f* accel, f32 scale, s16 timer);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0F544.s")
 
+// void en_bsb_eff_move(EnBsb* this, PlayState* play);
+void func_80C0F640(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0F640.s")
 
+// void en_bsb_eff_disp(EnBsb* this, PlayState* play);
+void func_80C0F758(EnBsb* this, PlayState* play);
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_En_Bsb/func_80C0F758.s")
--- src/overlays/actors/ovl_En_Bsb/z_en_bsb.h
+++ src/overlays/actors/ovl_En_Bsb/z_en_bsb.h
@@ -2,18 +2,80 @@
 #define Z_EN_BSB_H
 
 #include "global.h"
+#include "assets/objects/object_bsb/object_bsb.h"
 
 struct EnBsb;
 
 typedef void (*EnBsbActionFunc)(struct EnBsb*, PlayState*);
 
+typedef struct {
+    /* 0x00 */ u8 type;
+    /* 0x04 */ Vec3f pos;
+    /* 0x10 */ Vec3f velocity;
+    /* 0x1C */ Vec3f accel;
+    /* 0x28 */ Vec3s workAngle;
+    /* 0x30 */ f32 scale;
+    /* 0x34 */ s16 timer;
+} CaptainKeetaEffect;
+
 typedef struct EnBsb {
     /* 0x0000 */ Actor actor;
-    /* 0x0144 */ char unk_144[0x140];
+    /* 0x0144 */ SkelAnime skelAnime;
+    /* 0x0188 */ Vec3s jointTable[OBJECT_BSB_LIMB_MAX];
+    /* 0x0206 */ Vec3s morphTable[OBJECT_BSB_LIMB_MAX];
     /* 0x0284 */ EnBsbActionFunc actionFunc;
-    /* 0x0288 */ char unk_288[0x54];
-    /* 0x02DC */ s32 unk2DC;
-    /* 0x02E0 */ char unk2E0[0xE78];
+    /* 0x0288 */ Path* path;
+    /* 0x028C */ s32 pathCount;
+    /* 0x0290 */ u8 hint_sound_flag;
+    /* 0x0292 */ s16 moveTimer;
+    /* 0x0294 */ s16 waitTimer;
+    /* 0x0296 */ s16 unk_296;
+    /* 0x0298 */ Vec3f damage_spd;
+    /* 0x02A4 */ s32 sundry_count;
+    /* 0x02A8 */ s32 sundry_flag;
+    /* 0x02AC */ s16 sundry_angle;
+    /* 0x02AE */ u8 dust_flag;
+    /* 0x02AF */ u8 dush_bgm_flag;
+    /* 0x02B0 */ s32 index;
+    /* 0x02B4 */ s16 stat_mode;
+    /* 0x02B6 */ s16 rail_index;
+    /* 0x02B8 */ s16 start_bit;
+    /* 0x02BA */ s16 end_bit;
+    /* 0x02BC */ s16 angle_bak;
+    /* 0x02C0 */ f32 part_break_pos;
+    /* 0x02C4 */ f32 end_frame;
+    /* 0x02C8 */ s16 talk_count;
+    /* 0x02CA */ s16 body_alpha;
+    /* 0x02CC */ s16 Event_now[5];
+    /* 0x02D8 */ s32 c_change_flag;
+    /* 0x02DC */ s32 type_flag;
+    /* 0x02E0 */ Vec3f headPos;
+    /* 0x02EC */ Vec3f move_pos;
+    /* 0x02F8 */ Vec3f rightFootPos;
+    /* 0x0304 */ Vec3f leftFootPos;
+    /* 0x0310 */ Vec3s right_hand_angle;
+    /* 0x0316 */ Vec3s leftHandRot;
+    /* 0x031C */ Vec3s break_rot_angle;
+    /* 0x0322 */ s16 special_damage_timer;
+    /* 0x0324 */ s16 special_index;
+    /* 0x0328 */ f32 special_scale;
+    /* 0x032C */ f32 special_scale2;
+    /* 0x0330 */ Vec3f shape_pos[17];
+    /* 0x03FC */ s32 shape_jyoint_no[17];
+    /* 0x0440 */ s16 shape_count;
+    /* 0x0444 */ CaptainKeetaEffect effects[50];
+    /* 0x0F34 */ ColliderJntSph collider;
+    /* 0x0F54 */ ColliderJntSphElement colliderElements[7];
+    /* 0x1114 */ u32 demo_time;
+    /* 0x1118 */ s16 demo_stat;
+    /* 0x111A */ s16 camNum;
+    /* 0x111C */ s16 demo_count;
+    /* 0x1120 */ f32 fovy;
+    /* 0x1124 */ f32 fovy_max;
+    /* 0x1128 */ Vec3f eye;
+    /* 0x1134 */ Vec3f way;
+    /* 0x1140 */ Vec3f eye_max;
+    /* 0x114C */ Vec3f way_max;
 } EnBsb; // size = 0x1158
 
 #endif // Z_EN_BSB_H
